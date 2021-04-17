029c5533e37cfd1d6d721cc1b3e6a5848848f515
root <root@NemerTamimiPC.localdomain>
2021-04-17 17:57:28 +0300

    s

---
+++ README.md
@@ -0,0 +1,15 @@
+# Semaphore Game
+
+
+## executing
+
+
+```
+gcc semaphore.c -o semaphore
+gcc main.c -o main
+./main
+```
+
+### Nemer Tamimi 
+
+#### 1170025
---
+++ main.c
@@ -0,0 +1,45 @@
+// colors
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
+// end of colors
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int i = 0;
+int main()
+{
+    for (i = 0; i < 6; i++)
+    {
+
+        pid_t c = fork();
+        if (c == 0) // fork returns 0 to the child process so it enters "if" block
+        {
+            if (i == 0)
+            {
+                printf("\n\n \t--------------%sSemaphores game started%s---------------\n", KGRN, KNRM);
+                printf("%s\t  --------------------------------------------------%s\n\n", KRED, KNRM);
+            }
+            char str[1];
+            sprintf(str, "%d", i);
+            char *arg_list[] = {"semaphore", str, NULL};
+            if (execvp("./semaphore", arg_list) == -1)
+            {
+                perror("executing_error");
+                exit(1);
+            }
+            else
+            {
+                exit(0);
+            }
+        }
+    }
+
+    return 0;
+}
